49219ecb9e24389a3ea7a8863661fcef2bd325b0
Joel Gama <joelgama2015@gmail.com>
2020-05-30 18:50:41 +0100

    Protótipo da função "ajuda"; ficheiro com informação de ajuda.

---
+++ printHelp.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int printHelp(){
+	
+	int i = 0;
+	char *buf[1024];
+
+	int fd = open("help.txt", O_CREAT | O_WRONLY, 0600);
+
+	while (i = read(fd,buf,1024)){
+        bytes += i;     ;
+    }
+
+	close(fd);
+
+	write(1,buf,i);
+
+	return 0;
+}
