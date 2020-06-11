dfb9f326ed4874605cd1120e3c3d175448acaf78
JoelGama <joelgama2015@gmail.com>
2020-06-11 22:43:54 +0100

    Corrigir executar (Funciona com o primeiro comando, falta fazer ciclo)

--- auxiliares.h
+++ auxiliares.h
@@ -4,11 +4,12 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/wait.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <fcntl.h>
 
-char** split(char* command);
+char** split(char* command, char* s);
 
 int printHelp();
 #endif
--- execute.c
+++ execute.c
@@ -1,20 +1,14 @@
-#include <sys/wait.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "auxiliares.h"
 
-char *strtok(char *str, const char *delim);
-
-static char** split(char* command){
+char** split(char* command, char* s){
 
     int i = 0;
     char** argv = malloc(sizeof(char*) * 100);
 
-    char* c = strtok(command, " ");
+    char* c = strtok(command,s);
     while(c){
         argv[i++] = strdup(c);
-        c = strtok(NULL," ");  
+        c = strtok(NULL,s);  
     }
 
     argv[i] = NULL;
@@ -22,116 +16,67 @@ static char** split(char* command){
     return argv;
 }
 
-int executar(char* command) {
+int executar(char *command) {
 
     int status;
     int i = 1;
     int j = 0;
     pid_t pid;
-    int cc = 0;
-    int count = 0;
-    char *cmds[20];
-    char *cmd[20];
 
-    char** argv = malloc(sizeof(char*) * 100);
+    char** cmds = malloc(sizeof(char*) * 50);
+    char** argv = malloc(sizeof(char*) * 10);
 
-    char* str;
-    char* a;
-    char aux[50];
-    strcpy(aux,command);
+    printf("Comando input: %s\n",command);
 
-    for(i = 0; aux[i] != '\0'; i++) {
+    int fd[2];
 
-        if (aux[i+1] == '\0'){
-            a = aux[i];
-            strcat(str,a);
-            cmds[j++] = str;
-            printf("%s\n",str);
-            break;
-        }
-
-        if (aux[i] == '|'){
-            cmds[j++] = str;
-            printf("%s\n",str);
-            str = "";
-        } else {
-
-        a = strcpy(a,aux[i]);
-        trcat(str,a);
-        }
+    if(pipe(fd) < 0) {
+        perror("couldn't pipe");
+        exit(EXIT_FAILURE);
     }
-    i-1;
-    cmds[i] = NULL;
-    cc = i;
 
-    int pipefds[2*cc];
-
-    for(i = 0; i < cc; i++){
-        if(pipe(pipefds + i*2) < 0) {
-            perror("couldn't pipe");
-            exit(EXIT_FAILURE);
-        }
-    }
+    cmds = split(command,"|");
 
     j = 0;
-    while(cmds[count] != NULL) {
-        pid = fork();
-        if(pid == 0) {
+    if((pid = fork()) == 0) {
 
-            //if not last command
-            if(cmds[count+1] != NULL){
-                if(dup2(pipefds[j + 1], 1) < 0){
-                    perror("dup2");
-                    exit(EXIT_FAILURE);
-                }
-            }
-            count--;
-            //if not first command&& j!= 2*numPipes
-            if(count != 0 ){
-                if(dup2(pipefds[j-2], 0) < 0){
-                    perror(" dup2");///j-2 0 j+1 1
-                    exit(EXIT_FAILURE);
+        dup2(fd[0],0);
 
-                }
-            }
+        close(fd[0]);
+        close(fd[1]);
 
-            for(i = 0; i < 2*cc; i++){
-                close(pipefds[i]);
-            }
+        argv = split(command," ");
 
-            argv = split(cmds[count]);
-
-            printf("argv[%d]: %s\n",count,(char*)argv[0]);
-
-            if(execvp(argv[0], argv) < 0){
-                    perror(argv[0]);
-                    exit(EXIT_FAILURE);
-            }
-
-        } else if(pid < 0){
-            perror("error");
+        if(execvp(argv[0], argv) < 0){
+            perror(argv[0]);
             exit(EXIT_FAILURE);
         }
 
-        count++;
-        j+=2;
+    } else 
+        if(pid < 0){
+            perror("error");
+            exit(EXIT_FAILURE);
+    }
+ 
+    for(i = 0; i < 2; i++){
+        close(fd[i]);
     }
 
-    /**Parent closes the pipes and wait for children*/
-    for(i = 0; i < 2 * cc; i++){
-        close(pipefds[i]);
-    }
-
-    for(i = 0; i < cc + 1; i++)
-        wait(&status);
+    wait(&status);
 
     return 0;
 }
 
-
 int main(int argc, char const *argv[]){
     
-    int i = executar((char*)argv[1]);
+    char* cmd = malloc(sizeof(char*) * 100);
+
+    for(int i = 1; i < argc; i++){
+        strcat(cmd,argv[i]);
+        strcat(cmd," ");
+    }
+
+    int i = executar(cmd);
 
     return i;
 }
